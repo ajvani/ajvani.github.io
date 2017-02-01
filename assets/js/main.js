162965afde11afd0ef4f8ae9373b71203e95ea76
ajvani <anmol.jadvani@gmail.com>
2017-02-01 16:24:28 -0500

    small update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,19 @@ $(document).ready(function(){
 	    return false;
 	});	
 });
+
+function toggle(id) {
+    var elements = ["education", "skills", "links", "contact"]; 
+    var a = document.getElementById(id); 
+    if (a.style.display == 'none') {
+        var i; 
+        for (i = 0; i < elements.length; i++) {
+            if (elements[i] != id) {
+                document.getElementById(elements[i]).style.display = 'none';
+            }
+        }
+        a.style.display = 'block'; 
+    } else {
+        a.style.display = 'none'; 
+    }
+}
